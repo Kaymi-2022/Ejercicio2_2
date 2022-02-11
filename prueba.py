65e24a033cad4cd3fad5d79dc51334a63c2fe23d
Admin <Admin@dna018.fap.mil.pe>
2022-02-11 10:04:35 -0500

    TERMINADO

--- prueba.py
+++ prueba.py
@@ -9,7 +9,6 @@ class Ejemplo01(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         uic.loadUi("Principal.ui",self)
-        self.b_borrar.clicked.connect(self.reset)
         self.b_salir.clicked.connect(self.exit)
         self.b_ciclo1.clicked.connect(self.ciclo1)
         self.b_ciclo2.clicked.connect(self.ciclo2)
@@ -18,18 +17,6 @@ class Ejemplo01(QMainWindow):
         self.b_ciclo5.clicked.connect(self.ciclo5)
         self.b_ciclo6.clicked.connect(self.ciclo6)
 
-    def dato(self):
-        nombre=self.txt_nombres.text()
-        apellido=self.txt_apellidos.text()
-        dato=nombre+apellido
-
-
-
-    def reset(self):
-        self.txt_codigo.setText("")
-        self.txt_apellidos.setText("")
-        self.txt_nombres.setText("")
-    
     def exit(self):
         self.close()
         
@@ -75,19 +62,22 @@ class Ventana1(QDialog):
         self.lb_curso2.setText("WORD")
         self.lb_curso3.setText("EXCEL")
         self.lb_curso4.setText("ACCES")
-    super.__class__
-    
+
     def salir(self):
         self.close()
 
     def calcular(self):
+        nombre=self.txt_nombres.text()
+        apellido=self.txt_apellidos.text()
         n1=float(self.txt_nota1.text())
         n2=float(self.txt_nota2.text())
         n3=float(self.txt_nota3.text())
         n4=float(self.txt_nota4.text())
-        self.lb_alumno.setText(Ejemplo01.dato(self))
         self.lb_promedio.setText(str(round(operar.promedio(n1,n2,n3,n4),2)))
         self.lb_observacion.setText(operar.situacion(n1,n2,n3,n4))
+        dato=nombre+ " "+apellido
+        self.lb_alumno.setText(dato)
+        self.lb_ciclo.setText("I")
 
 class Ventana2(QDialog):
     def __init__(self):
@@ -104,12 +94,17 @@ class Ventana2(QDialog):
         self.close()
 
     def calcular(self):
+        nombre=self.txt_nombres.text()
+        apellido=self.txt_apellidos.text()
         n1=float(self.txt_nota1.text())
         n2=float(self.txt_nota2.text())
         n3=float(self.txt_nota3.text())
         n4=float(self.txt_nota4.text())
         self.lb_promedio.setText(str(round(operar.promedio(n1,n2,n3,n4),2)))
         self.lb_observacion.setText(operar.situacion(n1,n2,n3,n4))
+        dato=nombre+ " "+apellido
+        self.lb_alumno.setText(dato)
+        self.lb_ciclo.setText("II")
 
 class Ventana3(QDialog):
     def __init__(self):
@@ -126,12 +121,17 @@ class Ventana3(QDialog):
         self.close()
 
     def calcular(self):
+        nombre=self.txt_nombres.text()
+        apellido=self.txt_apellidos.text()
         n1=float(self.txt_nota1.text())
         n2=float(self.txt_nota2.text())
         n3=float(self.txt_nota3.text())
         n4=float(self.txt_nota4.text())
         self.lb_promedio.setText(str(round(operar.promedio(n1,n2,n3,n4),2)))
         self.lb_observacion.setText(operar.situacion(n1,n2,n3,n4))
+        dato=nombre+ " "+apellido
+        self.lb_alumno.setText(dato)
+        slef.lb_ciclo.setText("III")
 
 class Ventana4(QDialog):
     def __init__(self):
@@ -144,16 +144,22 @@ class Ventana4(QDialog):
         self.lb_curso3.setText("SQL-II")
         self.lb_curso4.setText("Analisis -II")
         
+        
     def salir(self):
         self.close()
 
     def calcular(self):
+        nombre=self.txt_nombres.text()
+        apellido=self.txt_apellidos.text()
         n1=float(self.txt_nota1.text())
         n2=float(self.txt_nota2.text())
         n3=float(self.txt_nota3.text())
         n4=float(self.txt_nota4.text())
         self.lb_promedio.setText(str(round(operar.promedio(n1,n2,n3,n4),2)))
         self.lb_observacion.setText(operar.situacion(n1,n2,n3,n4))
+        dato=nombre+ " "+apellido
+        self.lb_alumno.setText(dato)
+        self.lb_ciclo.setText("IV")
 
 class Ventana5(QDialog):
     def __init__(self):
@@ -170,12 +176,17 @@ class Ventana5(QDialog):
         self.close()
 
     def calcular(self):
+        nombre=self.txt_nombres.text()
+        apellido=self.txt_apellidos.text()
         n1=float(self.txt_nota1.text())
         n2=float(self.txt_nota2.text())
         n3=float(self.txt_nota3.text())
         n4=float(self.txt_nota4.text())
         self.lb_promedio.setText(str(round(operar.promedio(n1,n2,n3,n4),2)))
         self.lb_observacion.setText(operar.situacion(n1,n2,n3,n4))
+        dato=nombre+ " "+apellido
+        self.lb_alumno.setText(dato)
+        self.lb_ciclo.setText("V")
 
 class Ventana6(QDialog):
     def __init__(self):
@@ -192,12 +203,17 @@ class Ventana6(QDialog):
         self.close()
 
     def calcular(self):
+        nombre=self.txt_nombres.text()
+        apellido=self.txt_apellidos.text()
         n1=float(self.txt_nota1.text())
         n2=float(self.txt_nota2.text())
         n3=float(self.txt_nota3.text())
         n4=float(self.txt_nota4.text())
         self.lb_promedio.setText(str(round(operar.promedio(n1,n2,n3,n4),2)))
         self.lb_observacion.setText(operar.situacion(n1,n2,n3,n4))
+        dato=nombre+ " "+apellido
+        self.lb_alumno.setText(dato)
+        self.lb_ciclo.setText("VI")
 
 
 app = QApplication(sys.argv)
